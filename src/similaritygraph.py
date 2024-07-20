3d3174cf5b9fdf662123aacc00b71d311f5d4ea8
victor.litoux <victor.litoux@epita.fr>
2024-07-20 17:00:32 +0200

    Write into a GML

--- src/similaritygraph.py
+++ src/similaritygraph.py
@@ -9,17 +9,18 @@ delimiter = ';'
 
 
 class SimilarityGraph:
-	def __init__(self, graph: nx.DiGraph = nx.DiGraph(), show_display=True):
+	def __init__(self, graph: nx.DiGraph = None, show_display=True):
+		if graph is None:
+			graph = nx.DiGraph()
 		self.graph = graph
 		self.ploter = GraphPloter()
 		self.edhrec_asker = MyEDHREc()
-		self.count_cards = 0
 		self.max_add = 6 ** 5
 		self.display_bar: DisplayHandle | None = display(self._progress(),
 			display_id=True) if show_display else None
 
 	def _progress(self, card=None):
-		return HTML(f"Current card : {card}<br>Number of Cards : {self.count_cards}")
+		return HTML(f"Current card : {card}<br>Number of Cards : {len(self.graph.nodes)}")
 
 	def _update_display(self, text: str | HTML):
 		if self.display_bar is not None:
@@ -28,23 +29,28 @@ class SimilarityGraph:
 			self.display_bar.update(text)
 
 	@staticmethod
-	def load_graph(file_path: str):
-		return SimilarityGraph(nx.read_adjlist(file_path, delimiter=delimiter))
+	def load_graph(file_path: str, **kwargs):
+		return SimilarityGraph(graph=nx.read_gml(file_path), **kwargs)
 
 	def write_graph(self, file_path: str):
-		nx.write_adjlist(self.graph, file_path, delimiter=delimiter)
+		nx.write_gml(self.graph, file_path)
 
 	def add_card(self, card_name: str, depth_similar_cards: int = 1):
 		self._rec_add_card(card_name, depth_similar_cards)
 
+	def __len__(self):
+		return len(self.graph.nodes)
+
 	def _rec_add_card(self, card_name: str, depth_similar_cards: int):
 		similar_cards = self.edhrec_asker.get_similar_cards(card_name)
-		self.count_cards += 1
 		self._update_display(self._progress(card_name))
 		for card in similar_cards:
 			self.graph.add_edge(card_name, card["name"], weight=1)
-			if self.count_cards >= self.max_add:
+			if len(self) >= self.max_add:
 				self._update_display(
-					f"Max number of cards reached :  + {self.count_cards} / {self.max_add}")
+					f"Max number of cards reached :  + {len(self)} / {self.max_add}")
 			elif depth_similar_cards > 1 and self.graph.out_degree(card["name"]) == 0:
 				self._rec_add_card(card["name"], depth_similar_cards - 1)
+
+	def plot_graph(self):
+		return self.ploter.plot_graph(self.graph)
--- tests/test_similaritygraph.py
+++ tests/test_similaritygraph.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from unittest import TestCase
 
+import networkx.algorithms.isomorphism
+
 from src.similaritygraph import SimilarityGraph
 from tests import path_resources_tests
 
@@ -11,13 +13,19 @@ class TestSimilarityGraph(TestCase):
 			for line1, line2 in zip(f1, f2):
 				self.assertEqual(line1, line2)
 
-	def test_load_graph(self):
-		self.fail()
+	def assert_similar_graphs(self, graph1: SimilarityGraph, graph2: SimilarityGraph):
+		self.assertEquals(type(graph1.graph), type(graph2.graph))
+		self.assertEquals(graph1.graph.nodes, graph2.graph.nodes)
+		self.assertEquals(graph1.graph.edges, graph2.graph.edges)
+		self.assertTrue(networkx.algorithms.isomorphism.is_isomorphic(graph1.graph, graph2.graph))
 
-	def test_write_graph(self):
+	def test_write_load_graph(self):
 		simi_graph = SimilarityGraph(show_display=False)
-		simi_graph.add_card('Dusk // Dawn', 3)
-		self.assertGreaterEqual(simi_graph.count_cards, 7)
-		self.assertLess(simi_graph.count_cards, 36)
-		simi_graph.write_graph(path_resources_tests / 'test_write_graph.txt')
-		
+		simi_graph.add_card('Dusk // Dawn', 2)
+		self.assertLessEqual(len(simi_graph), 36)
+		self.assertGreaterEqual(len(simi_graph), 7)
+		simi_graph.write_graph(path_resources_tests / 'test_write_graph.gml')
+		other_graph = SimilarityGraph.load_graph(path_resources_tests / 'test_write_graph.gml',
+			show_display=False)
+
+		self.assert_similar_graphs(simi_graph, other_graph)
